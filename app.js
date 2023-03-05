35004e5e1aa2b4908bfee77aa7e3fa564484c884
uorii <kennyuorii@gmail.com>
2023-03-05 15:37:35 +0300

    done

---
+++ app.js
@@ -0,0 +1,35 @@
+import { Column } from "./column.js"
+
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const FONT_SIZE = 16
+
+const columns = []
+const columnsCount = canvas.width / FONT_SIZE
+
+for (let i = 0; i < columnsCount; i++) {
+    columns.push(new Column(i * FONT_SIZE, FONT_SIZE, canvas.height, context))
+}
+
+context.font = `bold ${FONT_SIZE}px monospace`
+
+let column = new Column(100, FONT_SIZE, canvas.height, context)
+
+function animate() {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    context.fillStyle = 'green'
+    columns.forEach(column => column.drawSymbol())
+   
+
+    setTimeout(() => requestAnimationFrame(animate), 50)
+}
+
+animate()
+
+console.log('hello Neo')
